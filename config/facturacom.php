9a1e08869389db1a1806fae8b616d4c72429717f
Luis Gonzalez <luisarmando1234@gmail.com>
2023-10-28 12:50:41 -0400

    Adding config file

--- config/facturacom.php
+++ config/facturacom.php
@@ -2,5 +2,9 @@
 
 // config for Inquid/LaravelFacturacom
 return [
-
+    'api_key' =>  env('FACTURACOM_API_KEY', ''),    
+    'secret_key' =>  env('FACTURACOM_SECRET_KEY', ''),
+    'fplugin' =>  env('FACTURACOM_FPLUGIN', '9d4095c8f7ed5785cb14c0e3b033eeb8252416ed'),
+    'path_pdf' => env('FACTURACOM_PATH_PDF',"cfdi_files/pdf"),
+    'path_xml' => env('FACTURACOM_PATH_XML',"cfdi_files/xml"),
 ];
